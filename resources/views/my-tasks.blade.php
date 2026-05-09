a64c750e3d3f49fbcf7f947aae1619a8390462a3
Brian Marandof <brianmasta23@gmail.com>
2026-05-09 23:44:12 +0900

    upgrade pemanggilan link icon

--- resources/views/my-tasks.blade.php
+++ resources/views/my-tasks.blade.php
@@ -4,7 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ \App\Models\AppSetting::agency()['app_name'] }} | Tugas Saya</title>
-        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="shortcut icon" href="/favicon.ico">
+        <link rel="icon" type="image/png" sizes="48x48" href="/esurat-48x48.png">
+        <link rel="apple-touch-icon" sizes="192x192" href="/esurat-192x192.png">
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
